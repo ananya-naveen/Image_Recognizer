dd7bde5559dd122b804e5601ff47ff5b65d036d1
ananya-naveen <84455616+ananya-naveen@users.noreply.github.com>
2021-10-11 18:44:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality:90
+});
+camera=document.getElementById("camera");
+Webcam.attach(camera);
+
+function takeSnapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML="<img id='captured_image' src='"+data_uri+"'>";
+    });
+}
+
+console.log("ml5 version: ",ml5.version);
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/opiQ9AbAz/model.json',modelLoaded);
+function modelLoaded(){
+    console.log("Model is Loaded");
+}
+
+function check(){
+    img=document.getElementById("captured_image");
+    classifier.classify(img,gotResult);
+}
+function gotResult(error,results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("resultObjectName").innerHTML=results[0].label;
+        confidence=results[0].confidence*100;
+        document.getElementById("resultObjectAccuracy").innerHTML=confidence.toFixed(0)+"%";
+    }
+}
